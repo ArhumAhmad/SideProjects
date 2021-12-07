b0c7d3fc137e6c6dc87f2fe4058de9807e1ec8dc
ArhumAhmad <nhlfan1001@gmail.com>
2021-12-07 10:04:13 -0500

    Update validate search tree.py

--- validate search tree.py
+++ validate search tree.py
@@ -20,25 +20,14 @@ class Node:
 
     def setRight(self, n):
         self.right = Node(n)
-
-def validate(node, lst):
-    if (node.getLeft() == None) and (node.getRight() == None):
-        return True
-
-    if node.getLeft() is not None:
-        if node.getLeft().getNum() > min(lst + [node.getNum()]):
-            return False
-        else:
-            l_val = validate(node.getLeft(), lst + [node.getNum()])
+        
+def helper(node: Node, min, max):
+    if node is None:
+        return True 
+    elif (min is not None and node.getNum < min) or (max is not None and node.getNum() > max):
+        return False
     else:
-        l_val = True
+        return helper(node.getLeft(), min, node.getNum) and helper(node.getRight(), node.getNum(), max)
 
-    if node.getRight() is not None:
-        if node.getRight().getNum() < min(lst + [node.getNum()]):
-            return False
-        else:
-            r_val = validate(node.getRight(), lst + [node.getNum()])
-    else:
-        r_val = True
-
-    return (l_val and r_val)
+def validate(node):
+    return helper(node)
